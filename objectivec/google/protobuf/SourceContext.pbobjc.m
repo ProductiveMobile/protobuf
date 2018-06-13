6bf79ad3c679d61ccea5b33ec802c90b1c432976
Mikhail Davydov <azproduction@users.noreply.github.com>
2018-06-13 12:38:50 +0200

    Update SourceContext.pbobjc.m

--- objectivec/google/protobuf/SourceContext.pbobjc.m
+++ objectivec/google/protobuf/SourceContext.pbobjc.m
@@ -16,7 +16,7 @@
 #if GPB_USE_PROTOBUF_FRAMEWORK_IMPORTS
  #import <Protobuf/SourceContext.pbobjc.h>
 #else
- #import "google/protobuf/SourceContext.pbobjc.h"
+ #import "SourceContext.pbobjc.h"
 #endif
 // @@protoc_insertion_point(imports)
 
