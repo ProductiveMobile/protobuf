eee00553d94a34e17b030f9a43e0f6e0fa360bf2
Mikhail Davydov <azproduction@users.noreply.github.com>
2018-06-13 12:39:05 +0200

    Update Struct.pbobjc.m

--- objectivec/google/protobuf/Struct.pbobjc.m
+++ objectivec/google/protobuf/Struct.pbobjc.m
@@ -18,7 +18,7 @@
 #if GPB_USE_PROTOBUF_FRAMEWORK_IMPORTS
  #import <Protobuf/Struct.pbobjc.h>
 #else
- #import "google/protobuf/Struct.pbobjc.h"
+ #import "Struct.pbobjc.h"
 #endif
 // @@protoc_insertion_point(imports)
 
